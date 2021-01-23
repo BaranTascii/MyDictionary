934c4b2c47c692907b0976ccbd542859bf790868
Baran Taşcı <baran_fearles@hotmail.com>
2021-01-23 16:18:59 +0300

    Add project files.

---
+++ Odev2/MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] arrayKey;
+        TValue[] arrayValue;
+
+        TKey[] tempKey;
+        TValue[] tempValue;
+
+        public MyDictionary()
+        {
+            arrayKey = new TKey[0];
+            arrayValue = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            tempKey = arrayKey;
+            tempValue = arrayValue;
+
+            arrayKey = new TKey[arrayKey.Length + 1];
+            arrayValue = new TValue[arrayValue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                arrayKey[i] = tempKey[i];
+            }
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                arrayValue[i] = tempValue[i];
+            }
+
+            arrayKey[arrayKey.Length - 1] = key;
+            arrayValue[arrayValue.Length - 1] = value;
+
+        }
+    }
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> Products = new MyDictionary<string, int>();
+            Products.Add("Masa", 250);
+            Products.Add("Sandalye", 75);
+            Products.Add("Koltuk", 700);
+            Products.Add("Sehpa", 200);
+            Products.Add("Dolap", 400);
+            Products.Add("Yatak", 1500);
+
+        }
+    }
+}
